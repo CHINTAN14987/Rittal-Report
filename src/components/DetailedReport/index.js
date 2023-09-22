abfcbc1addb898a08f1dbb66d8a14d698ddc05ab
CHINTAN14987 <92396521+CHINTAN14987@users.noreply.github.com>
2023-09-22 04:23:15 -0700

    table-col

--- src/components/DetailedReport/index.js
+++ src/components/DetailedReport/index.js
@@ -5,18 +5,45 @@ const Report = ({ data }) => {
   const [selectedRowKeys, setSelectedRowKeys] = useState([]);
   const [expandedRowKeys, setExpandedRowKeys] = useState([]);
   const onSelectChange = (newSelectedRowKeys) => {
-    console.log('selectedRowKeys changed: ', newSelectedRowKeys);
+    console.log("selectedRowKeys changed: ", newSelectedRowKeys);
     setSelectedRowKeys(newSelectedRowKeys);
   };
 
   const rowSelection = {
     selectedRowKeys,
     onChange: onSelectChange,
-  
   };
-  const columns = [...Object.keys(data?.[0]).map((item) => {
-    return { title: item, dataIndex: item, key: item };
-  })];
+const exclude=["sub" ]
+  const columns = [
+    ...Object.keys(data?.[0]).filter((item)=>item.indexOf(exclude) ===-1).map((item) => {
+     switch(item){
+      case "Business Name":
+        return {
+          title: item,
+          dataIndex: item,
+          key: item,
+          render: (text) => (
+            <a href={text} target="_blank" rel="noopener noreferrer">
+              {text}
+            </a>
+          ),
+        };
+        case "Status":
+          return {
+            title: item,
+            dataIndex: item,
+            key: item,
+            render: (text) => (
+              <div style={{background: text==="No Response" && "#bed9e5", height:"100%", width:"100%"}}>
+                {text}
+              </div>
+            ),
+          };
+      default:
+        return { title: item, dataIndex: item, key: item };
+     }
+    }),
+  ];
 
   return (
     <div>
@@ -27,16 +54,10 @@ const Report = ({ data }) => {
           expandedRowRender: (record) => (
             <>
               {console.log(record)}
-              {record?.sub && <Report data={[record.sub]  } />}
+              {record?.sub && <Report data={[record.sub]} />}
             </>
           ),
-          rowExpandable: (record) =>
-           {
-            if(record.sub && Object.keys(record.sub).length){
-              setExpandedRowKeys(record.id)
-            }
-           }
-       
+          rowExpandable: (record) => record.sub && Object.keys(record.sub).length
         }}
         dataSource={data}
       />
